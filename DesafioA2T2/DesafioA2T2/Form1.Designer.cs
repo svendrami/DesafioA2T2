ec6dce1ab083a0b401169788f590ff066cd91b3d
svendrami <62160419+svendrami@users.noreply.github.com>
2020-08-22 10:05:16 -0300

    Commit

---
+++ DesafioA2T2/DesafioA2T2/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace DesafioA2T2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.counterSimLbl = new System.Windows.Forms.Label();
+            this.counterNaoLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(55, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sim";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(219, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Não";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Você gostou de nosso atendimento?\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sim";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Não";
+            // 
+            // counterSimLbl
+            // 
+            this.counterSimLbl.AutoSize = true;
+            this.counterSimLbl.Location = new System.Drawing.Point(93, 214);
+            this.counterSimLbl.Name = "counterSimLbl";
+            this.counterSimLbl.Size = new System.Drawing.Size(0, 13);
+            this.counterSimLbl.TabIndex = 5;
+            // 
+            // counterNaoLbl
+            // 
+            this.counterNaoLbl.AutoSize = true;
+            this.counterNaoLbl.Location = new System.Drawing.Point(93, 242);
+            this.counterNaoLbl.Name = "counterNaoLbl";
+            this.counterNaoLbl.Size = new System.Drawing.Size(0, 13);
+            this.counterNaoLbl.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 275);
+            this.Controls.Add(this.counterNaoLbl);
+            this.Controls.Add(this.counterSimLbl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Pesquisa de satisfação";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label counterSimLbl;
+        private System.Windows.Forms.Label counterNaoLbl;
+    }
+}
+
---
+++ DesafioA2T2/DesafioA2T2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesafioA2T2
+{
+    public partial class Form1 : Form
+    {
+
+        public int counterSim = 0;
+        public int counterNao = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("Você clicou em SIM");
+            //incrementar o contador sim
+            counterSim += 1;
+            updateCountersLabels();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Você clicou em Não");
+            //incrementar o contador não
+            counterNao += 1;
+            updateCountersLabels();
+        }
+
+        private void updateCountersLabels()
+        {
+            counterSimLbl.Text = counterSim.ToString();
+            counterNaoLbl.Text = counterNao.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
